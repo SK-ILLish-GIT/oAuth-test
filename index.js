0eefa927072047763eeb8124e7b72ba920f0f1cf
SK-ILLish-GIT <sksahilparvez2000@gmail.com>
2023-11-03 18:23:58 +0530

    basdgu

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     $("#name").text(profile.getName());
     $("#email").text(profile.getEmail());
-    $("#image").attr('src', profile.getImageUrl());
     $(".data").css("display", "block");
     $(".g-signin2").css("display", "none");
 }
