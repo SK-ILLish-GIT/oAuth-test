c95d5b1e0e0af924b90323f12b3d8bc57ba0d673
SK-ILLish-GIT <sksahilparvez2000@gmail.com>
2023-11-03 18:34:29 +0530

    asdg

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
+// Select the elements using plain JavaScript
+var nameElement = document.getElementById("name");
+var emailElement = document.getElementById("email");
+var dataElement = document.querySelector(".data");
+var signInButton = document.querySelector(".g-signin2");
+
+// Function to handle sign-in
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    $("#name").text(profile.getName());
-    $("#email").text(profile.getEmail());
-    $(".data").css("display", "block");
-    $(".g-signin2").css("display", "none");
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
+  
 
+// Function to handle sign-out
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         alert("You have been signed out successfully");
-        $(".g-signin2").css("display", "block");
-        $(".data").css("display", "none");
+        signInButton.style.display = "block";
+        dataElement.style.display = "none";
+    });
+}
+
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
     });
 }
